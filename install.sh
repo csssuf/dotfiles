ecffef718b38484cb3dce83fee7786be909a18e1
James Forcier <csssuf@csssuf.net>
2017-10-27 21:21:54 -0400

    install oh-my-zsh via `git clone` instead of `cp`

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 export FILES=(.gitconfig .i3status.conf .vimrc .Xdefaults .zshrc)
-export DIRS=(.i3 .oh-my-zsh .vim)
+export DIRS=(.i3 .vim)
 
 for file in ${FILES[*]}; do
     echo "File ${file}"
@@ -19,6 +19,9 @@ for dir in ${DIRS[*]}; do
     echo "Directory ${dir}"
     cp -r ./$dir ~/
 done 
+
+# Install oh my zsh.
+git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 
 # Install NeoBundle.
 mkdir -p ~/.vim/bundle
