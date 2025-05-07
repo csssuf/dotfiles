a3c1e8a8214ed6bc2c014bc9c11c983e353251a2
James Forcier <csssuf@csssuf.net>
2025-05-06 23:13:47 -0400

    vim: initial migration to native Neovim/Lua

---
+++ vim/.config/nvim/lua/plugins/deoplete.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "Shougo/deoplete.nvim",
+        event = "VeryLazy",
+    },
+    {
+        "rust-lang/rust.vim",
+        ft = "rust",
+    },
+    {
+        "sebastianmarkow/deoplete-rust",
+        ft = "rust",
+    }
+}
---
+++ vim/.config/nvim/lua/plugins/vividchalk.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "tpope/vim-vividchalk",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd([[colorscheme vividchalk]])
+        end,
+    },
+}
+
